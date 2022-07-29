cb03e034a3d6f69a0b7aeae11234bcab9e8843de
MenghySanchez <menghy.sanchez@outlook.com>
2022-07-29 13:07:09 -0500

    add ejercicio 3 TODO FUNCIONA CORRECTAMENTE y correccion del git ignore

--- servicios/src/main/java/com/cmc/rest/servicios/ServicioPersona.java
+++ servicios/src/main/java/com/cmc/rest/servicios/ServicioPersona.java
@@ -1,14 +1,20 @@
 package com.cmc.rest.servicios;
 
+
+import com.cmc.rest.commons.ValidationException;
 import com.cmc.rest.entidades.Persona;
 
 public class ServicioPersona {
 
-	public static Persona actualizar(Persona p){
+	public static Persona actualizar(Persona p) throws ValidationException{
 	
-		p.setNombre(p.getNombre().toUpperCase());
-		p.setApellido(p.getApellido().toUpperCase());
-		
-		return p;
+		if(p.getNombre().length()<=3){
+			throw new ValidationException("el nombre es muy corto");
+		}if(p.getApellido().length()<=3){
+			throw new ValidationException("El apellido es muy corto");
+		}
+			p.setNombre(p.getNombre().toUpperCase());
+			p.setApellido(p.getApellido().toUpperCase());
+			return p;
 	}
 }
--- ws/src/java/com/cmc/wsrest/PersonaRest.java
+++ ws/src/java/com/cmc/wsrest/PersonaRest.java
@@ -6,7 +6,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.cmc.rest.commons.ValidationException;
 import com.cmc.rest.entidades.Persona;
 import com.cmc.rest.servicios.ServicioPersona;
 
@@ -23,10 +25,34 @@ public class PersonaRest {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Persona modificar(Persona per){
+	public Persona modificar(Persona per) throws ValidationException{
 		System.out.println(per);
 		Persona upPer = new Persona();
-		upPer =  ServicioPersona.actualizar(per);
+		try {
+			upPer =  ServicioPersona.actualizar(per);
+			return upPer;
+		} catch (ValidationException e) {
+			e.printStackTrace();
+		}
 		return upPer;
+	}
+	
+	
+	@Path("/cambio")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public static Response cambiar(Persona per){
+		
+		System.out.println(per);
+		Persona persona = null;
+		
+		try {
+			persona=ServicioPersona.actualizar(per);
+		} catch (ValidationException e) {
+			e.printStackTrace();
+			return Response.status(200).entity(e.getMessage()).build();
+		}
+		return Response.status(200).entity(persona).build();
 	}
 }
